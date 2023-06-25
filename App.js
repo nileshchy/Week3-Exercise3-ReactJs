453fb0665e6ae93ba89970f2b95c845365bf044b
Nilesh Kumar Choudhary <104972509+nileshchy@users.noreply.github.com>
2023-06-25 11:51:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NewComponent from './NewComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <NewComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ NewComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewComponent = () => {
+  return (
+    <div className="styled-div">
+      <h2>This is a Styled Component</h2>
+      <p>It has a light gray background color and dark gray text color.</p>
+    </div>
+  );
+};
+
+export default NewComponent;
